eb9d37235863ec42804984633555a55622c24200
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-05-16 00:42:42 -0500

    add more skeleton for the daily picker
    And then an idea was spawned about how to dynamically add new problem sets (like Euler) down the line, and I wanted to run off and chase that rabbit...

--- problem_picker/pick_problems.py
+++ problem_picker/pick_problems.py
@@ -4,12 +4,23 @@ from problem_picker.constants import SIMPLE_CATEGORIES
 from problem_picker.utils import grab_unsolved_problems
 
 
-def pick_problems() -> list:
+def pick_problems() -> None:
     '''
-    Pick a list of problems for the day.
+    Pick a set of 5 problems for the day.
 
-    Returns:
-        list[str]: The problems to solve for the day.
+    Notes:
+        - Tags are generated from the constants file.
+        - If a category is completed, please remove it from the tags list.
+        - If a category is to be added, please add it following the previous
+        template. (I do expect to add Euler one day.)
+        - A template directory is made for the selected problems.
+        - Please put unsolved problems from each day into `unsolved.txt`.
+        They will be recycled into the current day's choices, and their
+        template directory will be removed.
+        - 2 copies of the selected problems will be stored. One in a file for
+        the daily log, and one in `current_day` in root for ease of access.
+        On running this script, the previous day will have unsolved
+        problems removed.
     '''
     rosetta_problems = grab_unsolved_problems('rosetta_code')
 
