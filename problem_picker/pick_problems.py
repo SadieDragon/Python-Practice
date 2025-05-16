266f6080c38693b1811b9135242ca030268b2553
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-05-16 15:57:19 -0500

    empty out pick_problems
    It will be re-written once I finish the bulk of the script.

--- problem_picker/pick_problems.py
+++ problem_picker/pick_problems.py
@@ -1,9 +1,4 @@
 
-from pathlib import Path
-from problem_picker.constants import SIMPLE_CATEGORIES
-from problem_picker.utils import grab_unsolved_problems
-
-
 def pick_problems() -> None:
     '''
     Pick a set of 5 problems for the day.
@@ -22,12 +17,4 @@ def pick_problems() -> None:
         On running this script, the previous day will have unsolved
         problems removed.
     '''
-    rosetta_problems = grab_unsolved_problems('rosetta_code')
-
-    simple_problems = {}
-    for category in SIMPLE_CATEGORIES:
-        category_tag = Path('simple_programming_problems') / category
-
-        simple_problems[category] = grab_unsolved_problems(category_tag)
-
-    return [rosetta_problems, simple_problems]  # Temp
+    pass
